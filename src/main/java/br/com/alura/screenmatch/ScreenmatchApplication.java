6ae386fa25c7a7827e8d4b9950f6e6176a581295
Gabriel de Oliveira Cardoso <108769102+Cardosogoc@users.noreply.github.com>
2025-05-25 09:00:43 -0300

    Coletando dados de Temporadas e Episodios

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -20,7 +25,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=64cbf238&i=tt0238784");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
+
+		//Dados da Série
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		//Dados do Episódio
+		json =consumoApi.obterDados("https://www.omdbapi.com/?apikey=64cbf238&t=gilmore+girls&season=1&episode=2");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<= dados.totalTemporadas();i++){
+			json =consumoApi.obterDados("https://www.omdbapi.com/?apikey=64cbf238&t=gilmore+girls&season=" + i);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
